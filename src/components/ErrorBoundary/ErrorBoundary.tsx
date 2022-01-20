5928f78041343d531130097c0ffd88e87977719e
KRY-Pedro-Ferrari <pedro.ferrari@kry.se>
2022-01-20 22:45:52 +0100

    chore: update deprecated setting

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -12,7 +12,8 @@ export const ErrorBoundary: FC<Props> = ({ children, reset }) => (
     onReset={() => {
       // Reset the state and cleanup the memory of the app
       reset()
-    }}>
+    }}
+  >
     {children}
   </ReactErrorBoundary>
 )
